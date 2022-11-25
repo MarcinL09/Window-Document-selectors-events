1a7f85cda4ec6a9dd0ea7ec9bbd9ef3a37c2b14b
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-11-25 08:33:28 +0100

    added a button to HTML and change its background color

---
+++ index.js
@@ -0,0 +1,6 @@
+// Exercise: add a button to HTML and change its background color
+
+const buttonElement = document.querySelector('button');
+if (buttonElement) {
+    buttonElement.style.background = 'orange';
+}
