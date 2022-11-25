9ab1f4cbf10651b5b2958789388dde97432937b4
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-11-25 12:22:41 +0100

    added a class to all <p> elements that underlines the text

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const buttonElement = document.querySelector('button');
 if (buttonElement) {
     buttonElement.style.background = 'orange';
 }
+
+// Exercise: make the input disabled using JavaScript
+
+const inputElement = document.getElementById("#name");
+if (inputElement) {
+    inputElement.disabled = true;
+}
+
+// Exercise: add a class to all <p> elements that underlines the text
+
+const text = document.querySelectorAll('p');
+
+text.forEach(function(text){
+    text.classList.add("underlined-text");
+})
